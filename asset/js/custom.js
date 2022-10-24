d9bb81687031bb132b573bb88bcf3a94c7533919
devshisir <contact.shisirimran@gmail.com>
2022-10-25 00:04:12 +0600

    page done without responsive

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
-    var school = $(window);
-    var page = $('html, body');
+    // data slider
+    $('.owl-carousel').owlCarousel({
+        items:6,
+        loop:true,
+        margin:0,
+        nav:false,
+        dots:true,
+        mouseDrag:false,
+        touchDrag:false,
+        autoplay: true,
+        slideTransition: 'linear',
+        autoplayTimeout: 6000,
+        autoplaySpeed: 6000,
+        autoplayHoverPause: false,
+    });
+
+    // pricing input toggle
+    $('.swithBox input[type="checkbox"]').click(function(){
+        if($(this).prop("checked") == true){
+            $('.pricingItem').addClass('active');
+        }
+        else if($(this).prop("checked") == false){
+            $('.pricingItem').removeClass('active');
+        }
+    });
+
+    
     
 });
