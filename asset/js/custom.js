18a4a36b8dbeaae17cafad7aba032274879db6aa
devshisir <contact.shisirimran@gmail.com>
2022-10-26 00:58:31 +0600

    working on mobile responsive

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // data slider
     $('.owl-carousel').owlCarousel({
-        items:6,
+        items:5,
         loop:true,
         margin:0,
         nav:false,
@@ -13,6 +13,26 @@ $(document).ready(function() {
         autoplayTimeout: 6000,
         autoplaySpeed: 6000,
         autoplayHoverPause: false,
+        responsive : {
+            0:{
+                items: 1,
+            },
+            576:{
+                items: 2,
+            },
+            768 : {
+                items: 2,
+            },
+            992 : {
+                items: 3, 
+            },
+            1200:{
+                items: 4,
+            },
+            1400:{
+                items: 5,
+            }
+        }
     });
 
     // pricing input toggle
@@ -24,6 +44,54 @@ $(document).ready(function() {
             $('.pricingItem').removeClass('active');
         }
     });
+
+
+    // pricing select get value
+        $('.free').addClass('active')
+    $('.selectPriceOptions').on('change',function(e){
+        var val = $(this).find(":selected").val();
+        console.log(val)
+        var free = $('.free');
+        var startup = $('.startup');
+        var streamer = $('.streamer');
+        var professional = $('.professional');
+        var enterprisePrice = $('.enterprisePrice');
+        if(val == 1){
+            free.addClass('active')
+            startup.removeClass('active')
+            streamer.removeClass('active')
+            professional.removeClass('active')
+            enterprisePrice.removeClass('active')
+        }
+        if(val == 2){
+            startup.addClass('active')
+            free.removeClass('active')
+            streamer.removeClass('active')
+            professional.removeClass('active')
+            enterprisePrice.removeClass('active')
+        }
+        if(val == 3){
+            streamer.addClass('active')
+            startup.removeClass('active')
+            free.removeClass('active')
+            professional.removeClass('active')
+            enterprisePrice.removeClass('active')
+        }
+        if(val == 4){
+            professional.addClass('active')
+            streamer.removeClass('active')
+            startup.removeClass('active')
+            free.removeClass('active')
+            enterprisePrice.removeClass('active')
+        }
+        if(val == 5){
+            enterprisePrice.addClass('active')
+            professional.removeClass('active')
+            streamer.removeClass('active')
+            startup.removeClass('active')
+            free.removeClass('active')
+        }
+    })
 
     
     
